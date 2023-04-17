058139c2253a7265f9407baf4d5821a77954336b
Wang HoEun <hoeunwang@gmail.com>
2023-04-17 16:24:47 +0900

    feat: list component 구현

---
+++ src/components/List/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const List = styled.section`
+  flex: 1;
+  width: 100%;
+  margin: 1.5rem 0;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    background: none;
+    width: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #ffa50073;
+    border-radius: 1rem;
+  }
+`;
+
+export { List };
