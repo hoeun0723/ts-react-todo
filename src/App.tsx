114ec6738df3d9dd4d5b89bb962ac383c222f97c
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2023-04-17 15:54:46 +0900

    feat: input component 구현 (#8)
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Title from "./components/Title";
+import Container from "./components/Container";
+import Input from "./components/Input";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Title />
+      <Input />
     </div>
   );
 };
