d0229fe590b3464705a1c22b5653cab094ca8ac5
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2023-04-17 18:30:07 +0900

    feat: component 수정사항 반영 (#32)
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,11 @@ import Input from "./components/Input";
 import List from "./components/List";
 
 const App: React.FC = () => {
-  const items = [
-    {
-      text: "공부",
-    },
-    {
-      text: "산책",
-      done: true,
-    },
-    {
-      text: "잠자기",
-      done: true,
-    },
-  ];
   return (
     <div className="App">
       <Title />
       <Container>
-        <List items={items} />
+        <List />
         <Input />
       </Container>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import TodoStore from "./stores";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Provider store={TodoStore}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
