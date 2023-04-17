32db04da2024e1289729a45885f4d893a8b33b19
Wang HoEun <hoeunwang@gmail.com>
2023-04-17 18:20:13 +0900

    feat: list hooks 구현

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,33 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import * as S from "./style";
 import Item from "../Item";
 
-interface IListProps {
-  items: { text: string; done?: boolean }[];
-}
+import useTodos from "../../hooks/useTodos";
 
-const List = ({ items }: IListProps) => {
-  const [todos, setTodos] = useState(items);
-  const handleClickItem = (e: Event, index: number) => {
-    e.preventDefault();
-    const updatedTodos = todos.map((todo, i) =>
-      i === index ? { ...todo, done: !todo.done } : todo
-    );
-    setTodos(updatedTodos);
-  };
+const List = () => {
+  const todos = useTodos();
+
   return (
     <S.List>
-      {todos.map(({ text, done = false }, i) => {
-        return (
-          <Item
-            key={"todo" + i}
-            index={i}
-            text={text}
-            done={done}
-            onClick={handleClickItem}
-          />
-        );
-      })}
+      {todos.map((todo) => (
+        <Item todo={todo} key={todo.id} />
+      ))}
     </S.List>
   );
 };
