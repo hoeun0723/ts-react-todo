346a2e37c62209626287540046a2794ec51505e8
Wang Hoeun <38005874+hoeun0723@users.noreply.github.com>
2023-04-17 17:29:03 +0900

    feat: action에 따른 Reducer 구현 (#22)
    Co-authored-by: Wang HoEun <hoeunwang@gmail.com>

---
+++ src/reducers/index.ts
@@ -0,0 +1,24 @@
+import { createReducer } from "typesafe-actions";
+import { TodoAction, TodoState } from "../types";
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from "../actions/types";
+
+const initialState: TodoState = [
+  { id: 1, text: "공부", done: true },
+  { id: 2, text: "산책", done: true },
+  { id: 3, text: "잠", done: false },
+];
+
+const todos = createReducer<TodoState, TodoAction>(initialState, {
+  [ADD_TODO]: (state, { payload: text }) => [
+    ...state,
+    { id: Math.max(...state.map((todo) => todo.id)) + 1, text, done: false },
+  ],
+  [TOGGLE_TODO]: (state, { payload: id }) =>
+    state.map((todo) =>
+      todo.id === id ? { ...todo, done: !todo.done } : todo
+    ),
+  [REMOVE_TODO]: (state, { payload: id }) =>
+    state.filter((todo) => todo.id !== id),
+});
+
+export default todos;
